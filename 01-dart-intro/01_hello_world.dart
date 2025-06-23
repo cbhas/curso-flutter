0836ab3da94be3932e4df54b8e9bc4ac5e34b462
Sebastia <calderonsebastian211@gmail.com>
2025-06-23 14:50:19 -0500

    Class 01

---
+++ 01-dart-intro/01_hello_world.dart
@@ -0,0 +1,10 @@
+void main() {
+  // var myName = 'Sebastian';
+  // late final myName;
+  // String myName = 'Sebastian';
+  const myName = 'Sebastian';
+  
+  print('Hola $myName');
+  print('Hola ${ myName.toUpperCase() }');
+  print('Hola ${ 1 + 1 }');
+}
