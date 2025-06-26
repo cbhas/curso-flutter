9eb9c4864f28b397eb585b0b048d53ee40e5cb08
Sebastia <calderonsebastian211@gmail.com>
2025-06-25 20:20:36 -0500

    Class 05

--- 01-dart-intro/05_functions.dart
+++ 01-dart-intro/05_functions.dart
@@ -1,6 +1,7 @@
 void main() {
   print(greetEveryone());
   print('Suma: ${addTwoNumbers(10, 20)}');
+  print('${greetPerson(name: 'Sebastian', message: 'Bienvenido')}');
 }
 
 String greetEveryone() => 'Hello everyone!';
@@ -10,4 +11,8 @@ int addTwoNumbers(int a, int b) => a + b;
 int addTwoNumbersOptional(int a, [int b = 0]) {
   // b ??= 0;
   return a + b;
+}
+
+String greetPerson({required String name, String message = 'Hola,'}) {
+  return '$message $name';
 }
